8a3f37901b9393be99e2486e8f2b852867d0b7c4
INTONIGHT <tyler.throop@gmail.com>
2022-12-14 12:57:52 -0700

    more rust practice

---
+++ chapter_13/target/debug/chapter_13.d
@@ -0,0 +1,1 @@
+C:\Users\tyler\Desktop\PracticeFolder\CodingPractice\chapter_13\target\debug\chapter_13.exe: C:\Users\tyler\Desktop\PracticeFolder\CodingPractice\chapter_13\src\main.rs
---
+++ chapter_13/target/debug/deps/chapter_13.d
@@ -0,0 +1,5 @@
+C:\Users\tyler\Desktop\PracticeFolder\CodingPractice\chapter_13\target\debug\deps\chapter_13.exe: src\main.rs
+
+C:\Users\tyler\Desktop\PracticeFolder\CodingPractice\chapter_13\target\debug\deps\chapter_13.d: src\main.rs
+
+src\main.rs:
